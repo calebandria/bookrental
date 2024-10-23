f2d1868f0d82650ec9965740708c01fea460963c
calebandria <kalebaandriamanaja@gmail.com>
2024-10-23 10:06:51 -0400

    modification en cours à continuer

---
+++ src/main/java/com/kaleba/bookrental/bookrental/controller/RegistrationController.java
@@ -0,0 +1,5 @@
+package com.kaleba.bookrental.bookrental.controller;
+
+public class RegistrationController {
+    
+}
---
+++ src/main/java/com/kaleba/bookrental/bookrental/dto/RegistrationDto.java
@@ -0,0 +1,52 @@
+package com.kaleba.bookrental.bookrental.dto;
+
+import jakarta.validation.constraints.NotBlank;
+
+public class RegistrationDto {
+    @NotBlank(message = "Firstname is required")
+    private String prenom;
+
+    @NotBlank(message = "Familyname is required")
+    private String nom;
+
+    @NotBlank(message ="username is required")
+    private String username;
+
+    @NotBlank(message ="Password is required")
+    private String password;
+
+    public String getPrenom(){
+        return prenom;
+    }
+
+    public void setPrenom(String prenom){
+        this.prenom = prenom; 
+    }
+
+    public String getNom(){
+        return nom;
+    }
+
+    public void setNom(String nom){
+        this.nom = nom; 
+    }
+
+    public String getPassword(){
+        return password;
+    }
+
+    public void setPassword(String password){
+        this.password = password; 
+    }
+
+    public String getUsername(){
+        return username;
+    }
+
+    public void setUsername(String username){
+        this.username = username; 
+    }
+
+
+    
+}
